85623d58161e3c007d2672dd97bd1bab0e196c11
Your NameUmmunnoor <you@example.comUthmankhaleelah2@gmail.com>
2022-10-05 04:26:54 -0700

    first commit

---
+++ ActionResult/Controllers/StudentController.cs
@@ -0,0 +1,80 @@
+﻿using ActionResult.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionResult.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Student.List());
+        }
+
+
+        public IActionResult Add(Student student)
+        {
+            ViewBag.message = "Added successfully";
+            ViewData["message"] = "Added successfully";
+            if(HttpContext.Request.Method == "POST")
+            {
+                var result = Student.Add(student);
+                if (result.Item1)
+                {
+                    return RedirectToAction("Index");
+                }
+                return Content(result.Item2);
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int id, Student student)
+        {
+            
+            if(HttpContext.Request.Method == "POST")
+            {
+                var result = Student.Edit(id, student);
+                return Json(new {status = result.Item1, message = result.Item2});
+            }
+            student = Student.Find(id);
+            if (student == null)
+            {
+                return StatusCode(404, "Student Not Found");
+            }
+            return View(student);
+        }
+
+        public IActionResult View(int id)
+        {
+            var result = Student.Find(id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+        public IActionResult Delete(int id)
+        {
+            var result = Student.Delete(id);
+            if (result.Item1)
+            {
+                return RedirectToRoute(new { controller = "Student", action = "Index" });
+            }
+            return NotFound(result.Item2);
+        }
+
+        public IActionResult RedirectToPage()
+        {
+            return Redirect("https://google.com");
+        }
+
+        public IActionResult GetFile()
+        {
+            return File(System.IO.File.ReadAllBytes("C:\\Users\\Abdulrazaq Olanite O\\Desktop\\Cost.pdf"), "application/pdf", "newdata.pdf");
+        }
+
+        public IActionResult Partial()
+        {
+            return PartialView("_PartialView");
+        }
+    }
+}
---
+++ ActionResult/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿namespace ActionResult.Models
+{
+    public class Student
+    {
+        private static readonly List<Student> students = new List<Student>();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+
+        public static (bool, string) Add(Student student)
+        {
+            var exits = students.Where(s => s.Email == student.Email).Any();
+            if (!exits)
+            {
+                
+                student.Id = students.Count() == 0 ? 1 : students.Max(s => s.Id) + 1;
+                students.Add(student);
+                return (true, "Student Added");
+            }
+            return (false, "Email Already exit");
+        }
+
+        public static (bool, string) Edit(int id, Student updatedStudent)
+        {
+            var student = Find(id);
+            if(student != null)
+            {
+                student.FirstName = updatedStudent.FirstName;
+                student.LastName = updatedStudent.LastName;
+                student.Email = updatedStudent.Email;
+                return (true, "Student Updated");
+            }
+            return (false, "Student Not Found");
+        }
+        public static (bool, string) Delete(int id)
+        {
+            var student = Find(id);
+            if(student == null)
+            {
+                return (false, "Studnet Not found");
+            }
+            students.Remove(student);
+            return (true, "Student Removed");
+        }
+
+        public static List<Student> List()
+        {
+            return students;
+        }
+        public static Student Find(int Id)
+        {
+            return students.Where(s => s.Id == Id).SingleOrDefault();
+        }
+    }
+}
